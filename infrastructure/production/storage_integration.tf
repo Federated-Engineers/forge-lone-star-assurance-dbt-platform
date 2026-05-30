ca526e0c25edf69be16f73033e1a27da0f4bd1ee
unknown <ojokayode13@gmail.com>
2026-05-30 13:22:45 +0100

    Uncommented the storage integration

--- infrastructure/production/storage_integration.tf
+++ infrastructure/production/storage_integration.tf
@@ -1,4 +1,3 @@
-/*
 resource "snowflake_storage_integration_aws" "lonestar_s3" {
   name                      = "LONESTAR_S3_PRODUCTION"
   comment                   = "S3 integration for Lone Star Assurance production data lake"
@@ -7,4 +6,3 @@ resource "snowflake_storage_integration_aws" "lonestar_s3" {
   storage_aws_role_arn      = "arn:aws:iam::049417293525:role/forge-lonestar-snowflake-role"
   storage_allowed_locations = ["s3://lone-star-assurance-lake/"]
 }
-*/
