cd6e5bc5b866663ef6c7e04b378789eee549d747
unknown <ojokayode13@gmail.com>
2026-06-10 12:23:42 +0100

    Added Dockerfile to build image and also the dockerignore to exclude some files. Modified the run_dbt file

--- run_dbt.sh
+++ run_dbt.sh
@@ -4,11 +4,5 @@ set -e
 
 echo "Running DBT commands"
 
-echo "Run all dependencies to collect the required packages before dbt model execution"
-dbt deps --profiles-dir ./lonestar_edw --project-dir ./lonestar_edw
-
-echo "Run all model files holding business logic"
-dbt run --profiles-dir ./lonestar_edw --project-dir ./lonestar_edw
-
-echo "Run all predefined test cases"
-dbt test --profiles-dir ./lonestar_edw --project-dir ./lonestar_edw
+echo "Run all model files holding business logic and test"
+dbt build --profiles-dir ./lonestar_edw --project-dir ./lonestar_edw
