9f6b7028e8133b2fa7bd9b24d5f2fc9c4461bf9e
unknown <ojokayode13@gmail.com>
2026-06-11 11:43:58 +0100

    Removed unneccesary comments and added the dbt deps

--- run_dbt.sh
+++ run_dbt.sh
@@ -4,5 +4,8 @@ set -e
 
 echo "Running DBT commands"
 
+echo "Run all dependencies to collect the required packages before dbt model execution"
+dbt deps --profiles-dir ./lonestar_edw --project-dir ./lonestar_edw
+
 echo "Run all model files holding business logic and test"
 dbt build --profiles-dir ./lonestar_edw --project-dir ./lonestar_edw
