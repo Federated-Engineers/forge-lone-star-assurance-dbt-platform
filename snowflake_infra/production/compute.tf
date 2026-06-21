2c5dd7841b25b98a7556f3c0610af8da03fabdff
unknown <ojokayode13@gmail.com>
2026-06-21 17:45:20 +0100

    Corrected the compute fie to pass ci

--- snowflake_infra/production/compute.tf
+++ snowflake_infra/production/compute.tf
@@ -1,17 +1,17 @@
 module "etl_wh" {
   source = "../modules/warehouse"
 
-  name                 = "ETL_WH"
-  initially_suspended  = true
-  credit_quota         = 100
-  comment              = "Shared warehouse for the dbt and airflow service accounts"
+  name                = "ETL_WH"
+  initially_suspended = true
+  credit_quota        = 100
+  comment             = "Shared warehouse for the dbt and airflow service accounts"
 }
 
 module "data_eng_wh" {
   source = "../modules/warehouse"
 
-  name                 = "DATA_ENG_WH"
-  initially_suspended  = true
-  credit_quota         = 100
-  comment              = "Warehouse used by the data engineer role"
+  name                = "DATA_ENG_WH"
+  initially_suspended = true
+  credit_quota        = 100
+  comment             = "Warehouse used by the data engineer role"
 }
