62eafec196ac4bf8bbef38a32983ffbed3d49077
unknown <ojokayode13@gmail.com>
2026-07-02 20:01:18 +0100

    Fix the custumer staging model to pass ci test

--- lonestar_edw/models/staging/stg_customers.sql
+++ lonestar_edw/models/staging/stg_customers.sql
@@ -1,15 +1,15 @@
 select
-    c.value:customer_id::string        as customer_id,
-    c.value:first_name::string         as first_name,
-    c.value:last_name::string          as last_name,
-    c.value:dob::date                  as dob,
-    c.value:email::string              as email,
-    c.value:home_address::string       as home_address,
-    c.value:city::string               as city,
-    c.value:zip_code::string           as zip_code,
-    c.value:risk_segment::string       as risk_segment,
-    c.value:created_at::timestamp_ntz  as created_at,
+    c.value:customer_id::string as customer_id,
+    c.value:first_name::string as first_name,
+    c.value:last_name::string as last_name,
+    c.value:dob::date as dob,
+    c.value:email::string as email,
+    c.value:home_address::string as home_address,
+    c.value:city::string as city,
+    c.value:zip_code::string as zip_code,
+    c.value:risk_segment::string as risk_segment,
+    c.value:created_at::timestamp_ntz as created_at,
     src.source_file_name,
     src.load_timestamp
-from {{ source('bronze', 'lonestar_daily_load') }} src,
-     lateral flatten(input => src.lonestar_data:customers) c
+from {{ source('bronze', 'lonestar_daily_load') }} as src,
+    lateral flatten(input => src.lonestar_data:customers) as c
